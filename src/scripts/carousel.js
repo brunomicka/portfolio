e8c6e63e749a450cc619042f8a89aa464dd30eab
Bruno Michaelis <bruno.micka@gmail.com>
2021-09-01 17:45:50 -0500

    Styling image carousel + javascript

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -2,8 +2,22 @@ const carouselImg = document.getElementById('imgs')
 const imgs = document.querySelectorAll('#imgs img')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+const dotsUl = document.querySelector(".carousel-dots ul")
 
 let idx = 0
+
+imgs.forEach((img, idx) => {
+    const dotEl = document.createElement("li")
+    const dotBtn = document.createElement("button")
+
+    dotBtn.addEventListener('click', (idx) => {
+        changeImage(idx);
+    })
+
+    dotEl.appendChild(dotBtn)
+
+    dotsUl.appendChild(dotEl)
+})
 
 let interval = setInterval(() => {
     run()
@@ -26,12 +40,16 @@ function run() {
     changeImage()
 }
 
-function changeImage() {
+function changeImage(i) {
+    idx = i === undefined ? idx : i;
     if (idx > imgs.length - 1) {
         idx = 0
     } else if (idx < 0) {
         idx = imgs.length - 1
     }
+
+    console.log(idx)
+    
     carouselImg.style.transform = `translateX(-${idx * 700}px)`
 }
 
