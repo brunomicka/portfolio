ef4b1ffadf4fa50bb3932b3cce2f1f3dbdeffa8b
Bruno  Michaelis <brunomichaelis@Brunos-MacBook-Pro.local>
2021-09-01 22:47:30 -0500

    CSS and JS for Image Carousel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,5 +1,7 @@
 const carouselImg = document.getElementById('imgs')
+const carousel = document.querySelector('.carousel')
 const imgs = document.querySelectorAll('#imgs img')
+const arrows = document.querySelectorAll('.arrow')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 const dotsUl = document.querySelector(".carousel-dots ul")
@@ -10,14 +12,28 @@ imgs.forEach((img, idx) => {
     const dotEl = document.createElement("li")
     const dotBtn = document.createElement("button")
 
-    dotBtn.addEventListener('click', (idx) => {
+    if (idx === 0) {
+        dotBtn.classList.add('active')
+    }
+
+    dotBtn.addEventListener('click', () => {
         changeImage(idx);
     })
-
     dotEl.appendChild(dotBtn)
-
     dotsUl.appendChild(dotEl)
 })
+
+carousel.addEventListener('mouseover', () => {
+    arrows.forEach((arrow) => {
+        arrow.classList.add('show')
+    })
+});
+
+carousel.addEventListener('mouseout', () => {
+    arrows.forEach((arrow) => {
+        arrow.classList.remove('show')
+    })
+});
 
 let interval = setInterval(() => {
     run()
@@ -42,15 +58,27 @@ function run() {
 
 function changeImage(i) {
     idx = i === undefined ? idx : i;
+
     if (idx > imgs.length - 1) {
         idx = 0
     } else if (idx < 0) {
         idx = imgs.length - 1
     }
 
-    console.log(idx)
-    
+    clearActiveDots()
+
+    const nextLi = document.querySelectorAll(".carousel-dots ul li")[idx]
+
+    nextLi.firstElementChild.classList.add('active')
+
     carouselImg.style.transform = `translateX(-${idx * 700}px)`
+}
+
+function clearActiveDots() {
+    const dots = document.querySelectorAll(".carousel-dots ul li button")
+    dots.forEach((dot) => {
+        dot.classList.remove('active')
+    })
 }
 
 function resetInterval() {
