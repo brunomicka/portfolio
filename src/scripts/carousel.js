ac3b218b9755cf27d05e734bcec709418c54b5e8
Bruno  Michaelis <brunomichaelis@Brunos-MacBook-Pro.local>
2021-09-02 20:01:45 -0500

    Changing carousel javascript to work with multiple carousels at the same time

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,87 +1,98 @@
-const carouselImg = document.getElementById('imgs')
-const carousel = document.querySelector('.carousel')
-const imgs = document.querySelectorAll('#imgs img')
-const arrows = document.querySelectorAll('.arrow')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
-const dotsUl = document.querySelector(".carousel-dots ul")
+const carousels = document.querySelectorAll('.carousel')
+let carouselsObj = [];
 
-let idx = 0
-
-imgs.forEach((img, idx) => {
-    const dotEl = document.createElement("li")
-    const dotBtn = document.createElement("button")
-
-    if (idx === 0) {
-        dotBtn.classList.add('active')
-    }
-
-    dotBtn.addEventListener('click', () => {
-        changeImage(idx);
+carousels.forEach((carousel, i) => {
+    const arrows = carousel.querySelectorAll('.arrow')
+    const imgs = carousel.querySelectorAll('.imgs img')
+    const dotsUl = carousel.querySelector(".carousel-dots ul")
+    const leftBtn = carousel.querySelector('.previous button')
+    const rightBtn = carousel.querySelector('.next button')
+    
+    carouselsObj.push({
+        carousel,
+        idx: 0
     })
-    dotEl.appendChild(dotBtn)
-    dotsUl.appendChild(dotEl)
+
+    carousel.addEventListener('mouseover', () => {
+        arrows.forEach((arrow) => {
+            arrow.classList.add('show')
+        })
+    })
+
+    carousel.addEventListener('mouseout', () => {
+        arrows.forEach((arrow) => {
+            arrow.classList.remove('show')
+        })
+    })
+
+    imgs.forEach((img, idx) => {
+        const dotEl = document.createElement("li")
+        const dotBtn = document.createElement("button")
+
+        if (idx === 0) {
+            dotBtn.classList.add('active')
+        }
+
+        dotBtn.addEventListener('click', () => {
+            changeImage(carouselsObj[i], idx);
+        })
+
+        dotEl.appendChild(dotBtn)
+        dotsUl.appendChild(dotEl)
+    })
+
+    carouselsObj.interval = setInterval(() => {
+        run(carouselsObj[i])
+    }, (2000));
+
+    leftBtn.addEventListener('click', () => {
+        carouselsObj[i].idx--
+        changeImage(carouselsObj[i])
+        resetInterval(carouselsObj[i])
+    })
+
+    rightBtn.addEventListener('click', () => {
+        carouselsObj[i].idx++
+        changeImage(carouselsObj[i])
+        resetInterval(carouselsObj[i])
+    })
 })
 
-carousel.addEventListener('mouseover', () => {
-    arrows.forEach((arrow) => {
-        arrow.classList.add('show')
-    })
-});
-
-carousel.addEventListener('mouseout', () => {
-    arrows.forEach((arrow) => {
-        arrow.classList.remove('show')
-    })
-});
-
-let interval = setInterval(() => {
-    // run()
-}, (2000));
-
-leftBtn.addEventListener('click', () => {
-    idx--
-    changeImage()
-    resetInterval()
-})
-
-rightBtn.addEventListener('click', () => {
-    idx++
-    changeImage()
-    resetInterval()
-})
-
-function run() {
-    idx++
-    changeImage()
+const run = (carouselObj) => {
+    carouselObj.idx++
+    changeImage(carouselObj)
 }
 
-function changeImage(i) {
-    idx = i === undefined ? idx : i;
+const clearActiveDots = (carousel) => {
+    const dots = carousel.querySelectorAll(".carousel-dots ul li button")
 
-    if (idx > imgs.length - 1) {
-        idx = 0
-    } else if (idx < 0) {
-        idx = imgs.length - 1
-    }
-
-    clearActiveDots()
-
-    const nextLi = document.querySelectorAll(".carousel-dots ul li")[idx]
-
-    nextLi.firstElementChild.classList.add('active')
-
-    carouselImg.style.transform = `translateX(-${idx * carousel.offsetWidth}px)`
-}
-
-function clearActiveDots() {
-    const dots = document.querySelectorAll(".carousel-dots ul li button")
     dots.forEach((dot) => {
         dot.classList.remove('active')
     })
 }
 
-function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000);
+const changeImage = (carouselObj, i) => {
+    const imgs = carouselObj.carousel.querySelectorAll('.imgs img')
+    const imgContainer = carouselObj.carousel.querySelector('.image-container')
+
+    carouselObj.idx = i === undefined ? carouselObj.idx : i;
+
+    if (carouselObj.idx > imgs.length - 1) {
+        carouselObj.idx = 0
+    } else if (carouselObj.idx < 0) {
+        carouselObj.idx = imgs.length - 1
+    }
+
+    clearActiveDots(carouselObj.carousel)
+
+    const nextLi = carouselObj.carousel.querySelectorAll(".carousel-dots ul li")[carouselObj.idx]
+
+    nextLi.firstElementChild.classList.add('active')
+
+    imgContainer.style.transform = `translateX(-${carouselObj.idx * carouselObj.carousel.offsetWidth}px)`
+}
+
+const resetInterval = (carouselObj) => {
+    clearInterval(carouselObj.interval)
+    carouselObj.interval = setInterval(run(carouselObj), 2000);
 }
