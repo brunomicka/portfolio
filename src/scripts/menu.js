9ae802913a7859ed998b709b7dd5d3b542d5438b
Bruno Michaelis <bruno.micka@gmail.com>
2021-10-08 16:04:17 -0500

    colors and alignment - fix

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,9 +3,11 @@ const btnMobileClose = document.querySelector('.menu-close')
 const mobileMenu = document.querySelector('.menu')
 
 btnMobileMenu.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden'
     mobileMenu.classList.add('active')
 })
 
 btnMobileClose.addEventListener('click', () => {
+    document.body.style.overflow = ''
     mobileMenu.classList.remove('active')
 })
